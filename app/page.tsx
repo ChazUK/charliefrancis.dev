c54d0a2e4c006b4b7c9bfc2f7fafaa3ad1980d19
Charlie Francis <git@charliefrancis.dev>
2023-12-21 13:40:00 +0000

    feat: fitting video to text dimensions

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,25 @@ export default function Home() {
 
   return (
     <Fragment>
-      <video
-        className="fixed left-0 top-0 h-full w-full object-cover"
-        autoPlay
-        muted
-        loop
-        playsInline
-        src="/green-oil.mp4"
-      />
-      <main className="flex h-dvh w-full flex-col items-center justify-center gap-8 bg-black mix-blend-darken selection:bg-white selection:text-black">
-        <h1 className="text-5xl text-white md:text-9xl">
-          Charlie
-          <br />
-          Francis
-        </h1>
-        <Scramble lines={adjectives} pause={2000} />
+      <main className="flex h-dvh w-full items-center justify-center gap-8 bg-black selection:bg-white selection:text-black">
+        <div className="relative w-fit">
+          <video
+            className="absolute left-0 top-0 h-full w-full object-cover"
+            autoPlay
+            muted
+            loop
+            playsInline
+            src="/green-oil.mp4"
+          />
+          <div className="flex flex-col items-center justify-center gap-8 bg-black mix-blend-darken">
+            <h1 className="text-5xl text-white md:text-9xl">
+              Charlie
+              <br />
+              Francis
+            </h1>
+            <Scramble lines={adjectives} pause={2000} />
+          </div>
+        </div>
       </main>
     </Fragment>
   );
